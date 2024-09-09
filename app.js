0b8d8d129e67dd02cf2362cc320e123b31062e17
santtuss <SanttuAmi.Saaranen@metropolia.fi>
2024-09-09 14:48:40 +0300

    updated routes to contain /api/ + implemented morgan

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(morgan('tiny'));
 
 // Use the carRouter for all /cars routes
-app.use('/tours', tourRouter);
-app.use('/users', userRouter);
+app.use('/api/tours', tourRouter);
+app.use('/api/users', userRouter);
 
 const port = 4000;
 // Start the server
