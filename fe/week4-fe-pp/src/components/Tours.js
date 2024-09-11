561b605255e024913ee6a3ac704fe6e26f4c7d4f
emmkor <emmi.korhonen4@metropolia.fi>
2024-09-11 10:14:36 +0300

    add refactor tours

--- fe/week4-fe-pp/src/components/Tours.js
+++ fe/week4-fe-pp/src/components/Tours.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { tours } from "../data";
 import Tour from "./Tour.js";
 
 const Tours = () => {
+  const [toursData, setToursData] = useState(tours);
+  
   return (
     <section className="section" id="tour">
       <div className="section-title">
@@ -10,7 +13,7 @@ const Tours = () => {
         </h2>
       </div>
       <div className="section-center featured-center">
-        {tours.map((tour) => {
+        {toursData.map((tour) => {
           return <Tour {...tour} key={tour.id}/>
         })}
       </div>
